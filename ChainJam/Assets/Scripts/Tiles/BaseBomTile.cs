a87564376ed964b4e410e92f1a72c3c0c60d8252
JulianGrechEnriquez <julian.grech.enriquez@gmail.com>
2026-03-28 15:31:29 +0100

    added bomb

--- ChainJam/Assets/Scripts/Tiles/BaseBomTile.cs
+++ ChainJam/Assets/Scripts/Tiles/BaseBomTile.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public abstract class BaseBombTile : MonoBehaviour, IActionTile
 {
     public int bombLevel = 1;
     protected BoxCollider2D col;
+    protected Animator anim;
 
     protected virtual void Awake()
     {
         col = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void Action()
     {
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        DisableCollider();
+
+        if (anim != null)
+        {
+            anim.SetTrigger("explode");
+        }
+
+        yield break; // stop here, animation will handle the rest
+    }
+    public void OnExplodeAnimationEnd()
+    {
+        Debug.Log("EXPLOSION EVENT FIRED");
         Explode();
         Destroy(gameObject);
     }
 
-    protected abstract void Explode(); // each bomb defines this
+    protected abstract void Explode();
 
     protected void TriggerTile(Collider2D collider)
     {
